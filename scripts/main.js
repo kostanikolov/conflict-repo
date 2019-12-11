c2328c95a526be7c043bd60d01d3a5725a38356c
kostanikolov <kosta94@mail.bg>
2019-12-11 10:11:54 +0200

    added product to the solution

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,17 @@
 function solve(array) {
     let sum = 0;
+    let product = 1;
 
     for (let i = 0; i < array.length; i++) {
         sum += array[i];
     }
 
+    for (const el of array) {
+        product *= el;        
+    }
+
     console.log(sum);
+    console.log(product);
 }
 
 solve([11, 22, 33, 44]);
