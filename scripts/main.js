d1e255c1722465bcdda371f654510d7b8b24d56a
kostanikolov <kosta94@mail.bg>
2019-12-11 10:14:06 +0200

    added difference to the solution

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,17 @@
 function solve(array) {
     let sum = 0;
+    let diff = 0;
 
     for (let i = 0; i < array.length; i++) {
         sum += array[i];
     }
 
+    array.forEach(element => {
+        diff -= element;
+    });
+
     console.log(sum);
+    console.log(diff);
 }
 
 solve([11, 22, 33, 44]);
